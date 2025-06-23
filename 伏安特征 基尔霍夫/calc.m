e03a2b464c5ef43be0f71988696ee1bdda76aeb5
Mikasa <2411545@mail.nankai.edu.cn>
2025-06-23 20:52:35 +0800

    commit file

---
+++ 伏安特征 基尔霍夫/calc.m
@@ -0,0 +1,2 @@
+u=xlsread("data.xlsx","sheet1","C2:J2");
+i=xlsread("data.xlsx","sheet1","C3:J3");
---
+++ 叠加原理 戴维南定理 受控源/calc.m
@@ -0,0 +1,6 @@
+u1=xlsread("data.xlsx","sheet1","B22:J22");
+i2=xlsread("data.xlsx","sheet1","B23:J23");
+i1=xlsread("data.xlsx","sheet1","B30:I30");
+u2=xlsread("data.xlsx","sheet1","B31:I31");
+U1=xlsread("data.xlsx","sheet1","B38:K38");
+U2=xlsread("data.xlsx","sheet1","B39:K39");
